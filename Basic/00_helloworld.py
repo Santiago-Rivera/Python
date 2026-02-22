35971c34b081638433ca3e3cc93213025babb93f
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-02-22 15:33:34 -0500

    Add initial hello world script with type checking examples

---
+++ Basic/00_helloworld.py
@@ -0,0 +1,22 @@
+# Hola mundo #
+
+# Nuestro hola mundo en  Python #
+
+print("Hola mundo")
+
+print('Hola mundo')
+
+# Esto es un comentario
+
+""" Esto es un comentario de varias líneas
+que se puede usar para explicar el código
+o para escribir notas importantes """
+
+# Como consultar el tipo de dato #
+
+print(type("Soy un dato string"))
+print(type(42))
+print(type(3.14))
+print(type(3 + 4j))
+print(type(True))
+print(type(print("Mi cadena de texto")))
